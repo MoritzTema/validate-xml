1b034099d0599557b060282d4305a6c63b217b58
Moritz1404 <moritzsteinbusch@gmail.com>
2022-07-20 11:23:20 +0200

    sfd

--- validateUser.py
+++ validateUser.py
@@ -2,8 +2,10 @@ import os
 
 CHANGED_FILES = os.environ.get("CHANGED_FILES")
 CURRENT_USER = os.environ.get("CURRENT_USER")
+ALLOWED_USERS = os.environ.get("ALLOWED_USERS")
 
 
 print("Inside validateUser.py")
 print(CHANGED_FILES)
 print(CURRENT_USER)
+print(ALLOWED_USERS)
