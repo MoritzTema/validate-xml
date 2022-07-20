05cb356194930cf66b9005210be8603f2cf8e829
Moritz1404 <moritzsteinbusch@gmail.com>
2022-07-20 11:19:05 +0200

    asd

--- validateUser.py
+++ validateUser.py
@@ -1,6 +1,9 @@
 import os 
 
 CHANGED_FILES = os.environ.get("CHANGED_FILES")
+CURRENT_USER = os.environ.get("CURRENT_USER")
+
 
 print("Inside validateUser.py")
 print(CHANGED_FILES)
+print(CURRENT_USER)
